87772de99768e491d48c6cdfcf942458d842da3b
AlvaroEFMota <alvaro11318@gmail.com>
2020-07-24 22:23:56 -0300

    validaçao dos dados concluidos

--- myscripts.js
+++ myscripts.js
@@ -1,24 +1,82 @@
 function BinaryToDecimal() {
     var binary = document.getElementById("binary").value;
-    var decimal = parseInt(binary,2);
-    document.getElementById("decimal").value = decimal;
-    //console.log(decimal);
+        //console.log(binary);
+    if (binary != ""){
+        if(ValidateBinary(binary)) {
+            var decimal = parseInt(binary,2);
+            document.getElementById("decimal").value = decimal;
+            HideError("binary-msg");
+        } else {
+            console.log("erro");
+            ShowError("binary-msg");
+        }
+    } else{
+        HideError("binary-msg");
+        document.getElementById("decimal").value = "";
+    }
 }
 
 function DecimalToBinary() {
     var pilha = [];
     var decimal = document.getElementById("decimal").value;
     var binary = "";
-
-    decimal = parseInt(decimal)
-
-    while(decimal > 0) {
-         pilha.push(decimal%2);
-         decimal = parseInt(decimal/2);
+    if(decimal != ""){
+        if(ValidateDecimal(decimal)) {
+            HideError("decimal-msg");
+            decimal = parseInt(decimal)
+            if (decimal === 0){
+                pilha.push(parseInt(0));
+            }
+            while(decimal > 0) {
+                pilha.push(decimal%2);
+                decimal = parseInt(decimal/2);
+            }
+            binary = pilha.pop().toString();
+            while(pilha.length > 0) {
+                binary = binary.concat(pilha.pop().toString());       
+            }
+            document.getElementById("binary").value = binary;
+        } else {
+            ShowError("decimal-msg");
+        }
+    } else {
+        HideError("decimal-msg");
+        document.getElementById("binary").value = "";
     }
-    binary = pilha.pop().toString();
-    while(pilha.length > 0) {
-        binary = binary.concat(pilha.pop().toString());       
+}
+
+function ValidateBinary(binary) {
+    for(var i = 0; i < binary.length; i++) {
+        var number = parseInt(binary.charAt(i));
+        if (!isNaN(number)) {
+            if(parseInt(binary.charAt(i)) < 0 || parseInt(binary.charAt(i)) > 1) {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
-    document.getElementById("binary").value = binary;
+    return true;
+}
+
+function ValidateDecimal(decimal) {
+    for(var i = 0; i < decimal.length; i++) {
+        var number = parseInt(decimal.charAt(i));
+        if (!isNaN(number)) {
+            if(parseInt(decimal.charAt(i)) < 0 || parseInt(decimal.charAt(i)) > 9) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
+function ShowError(id) {
+    document.getElementById(id).style.display = "block";
+}
+
+function HideError(id) {
+    document.getElementById(id).style.display = "none";
 }
